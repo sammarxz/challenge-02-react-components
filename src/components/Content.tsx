0d36d2a9a8742186f420ce01eb1670edc467793e
Sam Marxz <sam@netmake.com.br>
2021-03-29 07:57:48 -0300

    resolved

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,29 @@
-export function Content() {
-  // Complete aqui
+import { MovieCard } from './MovieCard';
+
+import { GenreResponseProps } from "../@types/GenreResponseProps";
+import { MovieProps } from "../@types/MovieProps";
+
+import '../styles/content.scss';
+
+type ContentProps = {
+  selectedGenre: GenreResponseProps,
+  movies: MovieProps[]
+}
+
+export function Content({ selectedGenre, movies }: ContentProps) {
+  return (
+    <div className="container">
+      <header>
+        <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {movies.map(movie => (
+            <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+          ))}
+        </div>
+      </main>
+    </div>
+  )
 }
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,34 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from './Button';
+
+import { GenreResponseProps } from "../@types/GenreResponseProps";
+
+import '../styles/sidebar.scss';
+
+type SideBarProps = {
+  genres: GenreResponseProps[],
+  handleClickButton(id:number): void,
+  selectedGenreId: number
+}
+
+export function SideBar({ genres, selectedGenreId, handleClickButton }:SideBarProps) {
+  function onClickButton(id:number) {
+    handleClickButton(id)
+  }
+
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => onClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  )
 }
